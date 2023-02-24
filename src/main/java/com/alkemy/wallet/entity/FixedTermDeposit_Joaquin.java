2e4b4e4a4640c918897f82b4aa23b0ea46938594
Joaquin <joaquin.bonet@hasar.com>
2023-02-24 14:24:10 -0300

    JAVHASW1-14: Entidad FixedTermDeposit_Joaquin agregada

---
+++ src/main/java/com/alkemy/wallet/entity/FixedTermDeposit_Joaquin.java
@@ -0,0 +1,41 @@
+package com.alkemy.wallet.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "fixed_term_deposit", schema = "JAVHASW1")
+public class FixedTermDeposit_Joaquin {
+
+    @Id
+    @Column(name = "ID_FIXED_TERM_DEPOSIT", nullable = false)
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "Account")
+    @JoinColumn(name = "accountId", referencedColumnName = "IdAccount")
+    private UUID accountId;
+
+    @Column(name = "AMOUNT", nullable = false)
+    private double amount;
+    @Column(name = "INTEREST", nullable = false)
+    private double interest;
+    @Column(name = "CREATION_DATE", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp creationDate;
+    @Column(name = "CLOSING_DATE", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp closingDate;
+
+    public FixedTermDeposit_Joaquin(double amount, UUID accountId, double interest, Timestamp creationDate, Timestamp closingDate){
+        this.accountId = accountId;
+        this.amount = amount;
+        this.interest = interest;
+        this.creationDate = creationDate;
+        this.closingDate = closingDate;
+    }
+}
