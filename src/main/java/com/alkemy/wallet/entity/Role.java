a2731600d5fa19af9e758203f9b0349a5894f678
Joaquin <joaquin.bonet@hasar.com>
2023-02-24 14:33:58 -0300

    JAVHASW1-10: Entidad role agregada

---
+++ src/main/java/com/alkemy/wallet/entity/Role.java
@@ -0,0 +1,45 @@
+package com.alkemy.wallet.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "roles", schema = "JAVHASW1")
+public class Role {
+
+    @Id
+    @Column(name = "ID_ROLE", nullable = false)
+    private UUID roleId;
+    @Column(name="NAME", nullable = false, length = 80 )
+    @Enumerated(value = EnumType.STRING)
+    private Role name;
+    @Column(name = "DESCRIPTION", nullable = false, length = 100)
+    private String description;
+
+    @Column(name = "CREATION_DATE", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp creationDate;
+
+    @Column(name = "UPDATE_DATE", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp updateDate;
+
+    public enum name{
+        ADMIN,
+        USER;
+    }
+    public Role(UUID roleId, Role name, String description, Timestamp creationDate, Timestamp updateDate){
+        this.roleId = roleId;
+        this.name = name;
+        this.description = description;
+        this.creationDate = creationDate;
+        this.updateDate = updateDate;
+    }
+
+}
