d0af00ed52a5c0ea39b438abad7510258aaecc61
MatiasNSI <matias.islas@hasar.com>
2023-02-27 16:36:04 -0300

    JAVHASW1-11-v2: Agrego entidad Account

--- src/main/java/com/alkemy/wallet/entity/Account.java
+++ src/main/java/com/alkemy/wallet/entity/Account.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
 
 import javax.persistence.*;
 import java.sql.Time;
@@ -13,6 +14,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "accounts")
+@SQLDelete(sql = "UPDATE account set deleted = true WHERE id=?")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -44,10 +46,9 @@ public class Account {
     @Temporal(TemporalType.TIMESTAMP)
     private Timestamp updateDate;
 
-    @JoinColumn(name = "USER_ID", insertable = false, updatable = false)
+    private boolean softDelete = Boolean.FALSE;
+    @JoinColumn(name = "USER_ID")
     @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private User user;
-
-    @Column(name = "USER_ID", nullable = false)
-    private UUID userId;
+    
 }
